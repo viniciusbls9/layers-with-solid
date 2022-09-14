4f43a3401928989282b39981637e69e3ab4febab
Vinicius <beneditoloura@gmail.com>
2022-09-14 07:29:55 -0300

    feat: code before of to use SOLID principles

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 test("Deve criar uma transação", async function () {
-  const code = Math.floor(Math.random() * 1000);
+  const code = `${Math.floor(Math.random() * 1000)}`;
   axios({
     url: "http://localhost:3000/transactions",
     method: "POST",
@@ -12,4 +12,17 @@ test("Deve criar uma transação", async function () {
       paymentMethod: "credit_card",
     },
   });
+
+  const response = await axios({
+    url: `http://localhost:3000/transactions/${code}`,
+    method: "get",
+  });
+
+  const transaction = response.data;
+  expect(transaction.code).toBe(code);
+  expect(transaction.amount).toBe(1000);
+  expect(transaction.payment_method).toBe("credit_card");
+  expect(transaction.installments).toHaveLength(12);
+  expect(transaction.installments[0].amount).toBe(83.33);
+  expect(transaction.installments[11].amount).toBe(83.37);
 });
